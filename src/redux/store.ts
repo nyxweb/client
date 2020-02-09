4902b2609ed13ff8e74c1107350f1c6ec340fa7d
Dea7h <emil-n1k@hotmail.com>
2020-02-09 22:39:07 +0100

    fucking shit git

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducer from 'redux/reducers/admin';
+import rootReducer from 'redux/reducers';
 
 const store = createStore(
   rootReducer,
